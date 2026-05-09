8bd6094b9ea63dbf3187aaaa909df9e8d27beca7
kubaMiczuu <kubamiczuu@gmail.com>
2026-05-09 23:17:59 +0200

    feat: make 100% test coverage

--- src/test/java/org/jakubmiczek/model/BookTest.java
+++ src/test/java/org/jakubmiczek/model/BookTest.java
@@ -2,6 +2,9 @@ package org.jakubmiczek.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.*;
 
 
@@ -10,21 +13,58 @@ public class BookTest {
     public void shouldThrowExceptionWhenTitleIsEmpty() {
         assertThatThrownBy(() -> Book.builder().title("").build())
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Title cannot be empty");
+                .hasMessage("[SYSTEM]: Title cannot be empty");
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenTitleIsNull() {
+        assertThatThrownBy(() -> Book.builder().title(null).build())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[SYSTEM]: Title cannot be empty");
     }
 
     @Test
     public void shouldThrowExceptionWhenIsbnIsEmpty() {
         assertThatThrownBy(() -> Book.builder().title("test").isbn("").build())
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("ISBN cannot be empty");
+                .hasMessage("[SYSTEM]: ISBN cannot be empty");
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenIsbnIsNull() {
+        assertThatThrownBy(() -> Book.builder().title("test").isbn(null).build())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[SYSTEM]: ISBN cannot be empty");
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenPublishDateIsInvalid() {
+        assertThatThrownBy(() -> Book.builder().title("test").isbn("test").publishDate(LocalDate.parse("2028-04-03")).build())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[SYSTEM]: Publish date cannot be after now");
     }
 
     @Test
     public void shouldCreateCorrectBook() {
-        Book testBook = Book.builder().title("test").isbn("test").build();
+        Book testBook = Book.builder().title("test").isbn("test").publishDate(LocalDate.now()).build();
         assertThat(testBook.getId()).isNotNull();
         assertThat(testBook.getTitle()).isEqualTo("test");
         assertThat(testBook.getIsbn()).isEqualTo("test");
+    }
+
+    @Test
+    void shouldGenerateUUIDWhenIdIsNull() {
+        Book book = Book.builder().title("test").isbn("test").publishDate(LocalDate.now()).build();
+
+        assertThat(book.getId()).isNotNull();
+    }
+
+    @Test
+    void shouldUseProvidedUUID() {
+        UUID uuid = UUID.randomUUID();
+
+        Book book = Book.builder().id(uuid).title("test").isbn("test").publishDate(LocalDate.now()).build();
+
+        assertThat(book.getId()).isEqualTo(uuid);
     }
 }
--- src/test/java/org/jakubmiczek/service/LibraryServiceTest.java
+++ src/test/java/org/jakubmiczek/service/LibraryServiceTest.java
@@ -1,6 +1,7 @@
 package org.jakubmiczek.service;
 
 import org.jakubmiczek.exception.BookAlreadyExistException;
+import org.jakubmiczek.exception.BookNotFoundException;
 import org.jakubmiczek.model.Book;
 import org.jakubmiczek.repository.BookRepository;
 import org.junit.jupiter.api.Test;
@@ -9,9 +10,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,17 +28,73 @@ public class LibraryServiceTest {
 
     @Test
     public void shouldThrowBookAlreadyExistExceptionWhenIsbnExists() {
-        String  isbn = "123-456-7890";
 
-        Book newBook = Book.builder().title("newBook").isbn(isbn).build();
-        Book existingBook = Book.builder().title("existingBook").isbn(isbn).build();
+        String isbn = "123-456-7890";
+        Book newBook = Book.builder().title("newBook").isbn(isbn).publishDate(LocalDate.now()).build();
+        Book existingBook = Book.builder().title("existingBook").isbn(isbn).publishDate(LocalDate.now()).build();
 
         when(bookRepository.findByIsbn(isbn)).thenReturn(Optional.of(existingBook));
 
         assertThatThrownBy(() -> libraryService.addBook(newBook))
                 .isInstanceOf(BookAlreadyExistException.class)
-                .hasMessage("Book with ISBN " + isbn + " already exists");
+                .hasMessage("[SYSTEM]: Book with ISBN: " + isbn + " already exists");
 
         verify(bookRepository, never()).save(any());
+    }
+
+    @Test
+    public void shouldAddNewBook() {
+        String  isbn = "123-456-7890";
+        Book newBook = Book.builder().title("newBook").isbn(isbn).publishDate(LocalDate.now()).build();
+
+        when(bookRepository.findByIsbn(isbn)).thenReturn(Optional.empty());
+        libraryService.addBook(newBook);
+
+        verify(bookRepository, times(1)).save(newBook);
+    }
+
+    @Test
+    public void shouldThrowBookNotFoundExceptionWhenIsbnDoesNotExist() {
+        String  isbn = "123-456-7890";
+        when(bookRepository.findByIsbn(isbn)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> libraryService.deleteBook(isbn))
+                .isInstanceOf(BookNotFoundException.class)
+                .hasMessage("[SYSTEM]: Book with ISBN: "+isbn+ " not found");
+    }
+
+    @Test
+    public void shouldDeleteBook() {
+        String  isbn = "123-456-7890";
+        Book newBook = Book.builder().title("newBook").isbn(isbn).publishDate(LocalDate.now()).build();
+
+        when(bookRepository.findByIsbn(isbn)).thenReturn(Optional.of(newBook));
+        libraryService.deleteBook(isbn);
+
+        verify(bookRepository, times(1)).deleteByIsbn(isbn);
+    }
+
+    @Test
+    public void shouldReturnListOfBooksBasedOnTitleFragment() {
+        String fragment = "cat";
+
+        Book newBook1 = Book.builder().title("cat").isbn("test1").publishDate(LocalDate.now()).build();
+        Book newBook2 = Book.builder().title("Book").isbn("test2").publishDate(LocalDate.now()).build();
+        Book newBook3 = Book.builder().title("BookCat").isbn("test3").publishDate(LocalDate.now()).build();
+
+        when(bookRepository.findAll()).thenReturn(Arrays.asList(newBook1, newBook2, newBook3));
+
+        assertThat(libraryService.search(fragment)).hasSize(2);
+    }
+
+    @Test
+    public void shouldReturnAllBooks() {
+        Book newBook1 = Book.builder().title("cat").isbn("test1").publishDate(LocalDate.now()).build();
+        Book newBook2 = Book.builder().title("Book").isbn("test2").publishDate(LocalDate.now()).build();
+        Book newBook3 = Book.builder().title("BookCat").isbn("test3").publishDate(LocalDate.now()).build();
+
+        when(bookRepository.findAll()).thenReturn(Arrays.asList(newBook1, newBook2, newBook3));
+
+        assertThat(libraryService.getBooks()).hasSize(3);
     }
 }
